44605b04a13eb70d3520c8a2e18fb6a23341a3f7
Gibran Hemani <ghemani@login1.fornaxdomain>
2013-04-22 23:25:51 +0800

    updated script for fornax

--- run_0.95_unrel/run_perm_0.95_unrel.sh
+++ run_0.95_unrel/run_perm_0.95_unrel.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#PBS -A Desc006
-#PBS -l nodes=1:gpus=1
-#PBS -l pmem=4000MB
+#PBS -W group_list=director553
+#PBS -q workq
+#PBS -l select=1:ncpus=12:ngpus=1:mem=4000mb
+#PBS -l place=excl
 #PBS -N unrel95
-#PBS -t 1-500
+#PBS -t 251-500
 #PBS -l walltime=12:00:00
 #PBS -o job_reports/
 #PBS -e job_reports/
-#PBS -d /home/josephp/Desc006/repo/permutations/run_0.95_unrel
 
 set -e
 
 if [ -n "${1}" ]; then
-  PBS_ARRAYID=${1}
+  PBS_ARRAY_INDEX=${1}
 fi
-id=${PBS_ARRAYID}
+id=${PBS_ARRAY_INDEX}
 
-rtdir="/home/josephp/Desc006/repo/permutations/"
+rtdir="/home/ghemani/repo/permutations/"
 epiGPU="${rtdir}scripts/epiGPU"
 data="${rtdir}data/bsgs_1kg_p1v3_maf0.05_info0.95_chr5-15_unrel.egu"
 phen="${rtdir}data/phen_unrel.txt"
